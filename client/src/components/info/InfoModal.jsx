d9fc7b76484f97898373b002835ee0a9598e0308
Lukas <lukas.budginas@gmail.com>
2021-09-20 16:28:49 +0300

    info-edit

--- client/src/components/info/InfoModal.jsx
+++ client/src/components/info/InfoModal.jsx
@@ -6,13 +6,20 @@ export default function InfoModal({ show, close }) {
       <Modal centered show={show} onHide={close}>
         <Modal.Header>Info</Modal.Header>
         <Modal.Body className="info-modal-description">
-          Since we live in times when privacy is hard to achieve, I have created
-          this chat-app. This is anonymus and encrypted chat application, that
-          is why you need only your username and password. Anonymus means that
-          every message you send will reach database encrypted. What is more,
-          for extra security, every user can delete all messages from selected
-          conversation and from database at the same time. Please register and
-          follow instructions how to use it.
+          <p>
+            This is anonymus and encrypted chat application, that is why you
+            need only your username and password. Anonymus means that every
+            message you send will reach database encrypted. What is more, for
+            extra security, every user can delete all messages from selected
+            conversation and from database at the same time.
+          </p>
+          <h4>How to use it?</h4>
+          <p>
+            After registration, login window will appear. You must know your
+            friend's ID to start conversation, ID can be found at the top of the
+            page. After you find your friend, it will be added to conversations
+            section. Press "Open Conversations" and start chatting.
+          </p>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={close}>Close</Button>
